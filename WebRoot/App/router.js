9f7c833f0aa1413f03b3beb7fa581df3131875a1
dzy <duyud@qq.com>
2016-04-19 14:23:14 +0800

    del router

--- WebRoot/App/router.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by duyu on 2016/4/19.
- */
-var routes = {
-    '/login': author,
-    '/logout': [books, function() { console.log("An inline route handler."); }],
-    '/books/view/:bookId': viewBook
-};
