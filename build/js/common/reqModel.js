e65096b535a7c2fbd508ea1cf0a0ad75b6cc2fde
dzy <duyud@qq.com>
2016-05-26 14:18:30 +0800

    add req model

---
+++ build/js/common/reqModel.js
@@ -0,0 +1,19 @@
+define([], function(){
+	'use strict';
+
+	var exports = {};
+
+	exports.sort = function(name, type){
+		this.name = name;
+		this.type = type;
+	}
+
+	exports.reqModel = function(params, limit, offset, sort){
+		this.params = params;
+		this.limit = limit ? limit : 10;
+		this.offset = offset ? offset : 0;
+		this.sort = sort : sort : null;
+	} 
+
+	return exports;
+});
